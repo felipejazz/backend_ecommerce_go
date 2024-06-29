05694084aa7a40052ef5b0846bc381bff926c101
felipejazz <aluvinof@gmail.com>
2024-06-29 01:00:23 -0300

    initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/felipejazz/ecommerce_go/cmd/service/user"
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewServer(addr string, db *sql.DB) *Server {
+	return &Server{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *Server) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/server/v1").Subrouter()
+
+	log.Println("Listening on", s.addr)
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	return http.ListenAndServe(s.addr, router)
+}
